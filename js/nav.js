9685322353a0fd3e2f1ecbe380691049703ef365
jeeasistio <jeeasistio08@gmail.com>
2020-08-01 18:05:28 +0800

    nav optimized

--- js/nav.js
+++ js/nav.js
@@ -1,5 +1,7 @@
 const toggleNav = (navUL, navBtn) => {
   navBtn.addEventListener('click', () => navUL.classList.toggle('active-ul'));
+  
+  navUL.addEventListener('click', () => navUL.classList.toggle('active-ul'));
 }
 
 export default toggleNav;
